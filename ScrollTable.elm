ae5b4cd550841299a8fbdde8c7d33065a1cb8ce1
Justin Woo <moomoowoo@gmail.com>
2017-05-15 15:49:19 +0000

    make whitespace less jarring

--- ScrollTable.elm
+++ ScrollTable.elm
@@ -13,12 +13,12 @@ import List exposing (map, length, range)
 
 onScroll : (Int -> msg) -> Attribute msg
 onScroll tagger =
-    on "scroll" <| Json.map tagger scrollTop
+  on "scroll" <| Json.map tagger scrollTop
 
 
 scrollTop : Json.Decoder Int
 scrollTop =
-    Json.at [ "target", "scrollTop" ] Json.int
+  Json.at [ "target", "scrollTop" ] Json.int
 
 
 
@@ -26,7 +26,7 @@ scrollTop =
 
 
 type alias VisibleIndices =
-    List Int
+  List Int
 
 
 
@@ -35,20 +35,20 @@ type alias VisibleIndices =
 
 calculateVisibleIndices : Model -> Int -> Model
 calculateVisibleIndices model scrollTop =
-    let
-        { rowHeight, rowCount, height } =
-            model
+  let
+    { rowHeight, rowCount, height } =
+      model
 
-        firstRow =
-            max 0 <| scrollTop // rowHeight - 10
+    firstRow =
+      max 0 <| scrollTop // rowHeight - 10
 
-        visibleRows =
-            (height + 1) // rowHeight
+    visibleRows =
+      (height + 1) // rowHeight
 
-        lastRow =
-            min rowCount <| firstRow + visibleRows + 20
-    in
-        { model | visibleIndices = range firstRow lastRow }
+    lastRow =
+      min rowCount <| firstRow + visibleRows + 20
+  in
+    { model | visibleIndices = range firstRow lastRow }
 
 
 
@@ -56,13 +56,13 @@ calculateVisibleIndices model scrollTop =
 
 
 type alias Model =
-    { height : Int
-    , width : Int
-    , cellWidth : Int
-    , rowCount : Int
-    , rowHeight : Int
-    , visibleIndices : VisibleIndices
-    }
+  { height : Int
+  , width : Int
+  , cellWidth : Int
+  , rowCount : Int
+  , rowHeight : Int
+  , visibleIndices : VisibleIndices
+  }
 
 
 
@@ -70,18 +70,18 @@ type alias Model =
 
 
 type Msg
-    = NoOp
-    | UserScroll Int
+  = NoOp
+  | UserScroll Int
 
 
 update : Msg -> Model -> Model
 update action model =
-    case action of
-        NoOp ->
-            model
+  case action of
+    NoOp ->
+      model
 
-        UserScroll scrollTop ->
-            calculateVisibleIndices model scrollTop
+    UserScroll scrollTop ->
+      calculateVisibleIndices model scrollTop
 
 
 
@@ -89,103 +89,103 @@ update action model =
 
 
 type alias RowViewProps a =
-    { a
-        | rowHeight : Int
-        , cellWidth : Int
-    }
+  { a
+    | rowHeight : Int
+    , cellWidth : Int
+  }
 
 
 rowView : Int -> RowViewProps Model -> Html Msg
 rowView index props =
-    let
-        { rowHeight, cellWidth } =
-            props
+  let
+    { rowHeight, cellWidth } =
+      props
 
-        trStyle =
-            style
-                [ ( "position", "absolute" )
-                , ( "top", toString (index * rowHeight) ++ "px" )
-                , ( "width", "100%" )
-                ]
-    in
-        div [ trStyle ]
-            [ div
-                [ style
-                    [ ( "width", (toString cellWidth) ++ "px" )
-                    , ( "display", "inline-block" )
-                    ]
-                ]
-                [ text (toString (index))
-                ]
-            , div
-                [ style
-                    [ ( "width", (toString cellWidth) ++ "px" )
-                    , ( "display", "inline-block" )
-                    ]
-                ]
-                [ text (toString (index * 10))
-                ]
-            , div
-                [ style
-                    [ ( "width", (toString cellWidth) ++ "px" )
-                    , ( "display", "inline-block" )
-                    ]
-                ]
-                [ text (toString (index * 100))
-                ]
-            ]
+    trStyle =
+      style
+        [ ( "position", "absolute" )
+        , ( "top", toString (index * rowHeight) ++ "px" )
+        , ( "width", "100%" )
+        ]
+  in
+    div [ trStyle ]
+      [ div
+        [ style
+          [ ( "width", (toString cellWidth) ++ "px" )
+          , ( "display", "inline-block" )
+          ]
+        ]
+        [ text (toString (index))
+        ]
+      , div
+        [ style
+          [ ( "width", (toString cellWidth) ++ "px" )
+          , ( "display", "inline-block" )
+          ]
+        ]
+        [ text (toString (index * 10))
+        ]
+      , div
+        [ style
+          [ ( "width", (toString cellWidth) ++ "px" )
+          , ( "display", "inline-block" )
+          ]
+        ]
+        [ text (toString (index * 100))
+        ]
+      ]
 
 
 type alias TableViewProps a =
-    { a
-        | rowCount : Int
-        , rowHeight : Int
-        , visibleIndices : VisibleIndices
-    }
+  { a
+    | rowCount : Int
+    , rowHeight : Int
+    , visibleIndices : VisibleIndices
+  }
 
 
 tableView : TableViewProps Model -> Html Msg
 tableView props =
-    let
-        { rowCount, rowHeight, visibleIndices } =
-            props
+  let
+    { rowCount, rowHeight, visibleIndices } =
+      props
 
-        rows =
-            List.map
-                (\index ->
-                    rowView index props
-                )
-                visibleIndices
-    in
-        div [ style [ ( "height", toString (rowCount * rowHeight) ++ "px" ) ] ]
-            [ div [] rows
-            ]
+    rows =
+      List.map
+        (\index ->
+          rowView index props
+        )
+        visibleIndices
+  in
+    div [ style [ ( "height", toString (rowCount * rowHeight) ++ "px" ) ] ]
+      [ div [] rows
+      ]
 
 
 view : Model -> Html Msg
 view model =
-    let
-        { height, width, rowCount, rowHeight, visibleIndices } =
-            model
-    in
-        div []
-            [ h1 [ style [ ( "text-align", "center" ) ] ] [ text "Scroll Table!!!!" ]
-            , div [ id "app-container" ]
-                [ div
-                    [ class "scroll-table-container"
-                    , style
-                        [ ( "margin", "auto" )
-                        , ( "position", "relative" )
-                        , ( "overflowX", "hidden" )
-                        , ( "height", (toString height) ++ "px" )
-                        , ( "width", (toString width) ++ "px" )
-                        ]
-                    , onScroll UserScroll
-                    ]
-                    [ tableView model
-                    ]
-                ]
+  let
+    { height, width, rowCount, rowHeight, visibleIndices } =
+      model
+  in
+    div []
+      [ h1 [ style [ ( "text-align", "center" ) ] ] [ text "Scroll Table!!!!" ]
+      , div [ id "app-container" ]
+        [ div
+          [ class "scroll-table-container"
+          , style
+            [ ( "margin", "auto" )
+            , ( "position", "relative" )
+            , ( "overflowX", "hidden" )
+            , ( "height", (toString height) ++ "px" )
+            , ( "width", (toString width) ++ "px" )
             ]
+          , onScroll UserScroll
+          ]
+          [ tableView model
+          ]
+        ]
+      ]
 
 
 
@@ -194,29 +194,29 @@ view model =
 
 tableWidth : Int
 tableWidth =
-    900
+  900
 
 
 initialModel : Model
 initialModel =
-    { height = 500
-    , width = tableWidth + 200 -- extra leniency
-    , cellWidth = tableWidth // 3
-    , rowCount = 1000
-    , rowHeight = 30
-    , visibleIndices = []
-    }
+  { height = 500
+  , width = tableWidth + 200 -- extra leniency
+  , cellWidth = tableWidth // 3
+  , rowCount = 1000
+  , rowHeight = 30
+  , visibleIndices = []
+  }
 
 
 initializedModel : Model
 initializedModel =
-    calculateVisibleIndices initialModel 0
+  calculateVisibleIndices initialModel 0
 
 
 main : Program Never Model Msg
 main =
-    beginnerProgram
-        { model = initializedModel
-        , view = view
-        , update = update
-        }
+  beginnerProgram
+    { model = initializedModel
+    , view = view
+    , update = update
+    }
